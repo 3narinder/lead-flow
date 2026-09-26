689dfa703c4c07d63e2dd1acb851b6ce18e9fd89
3narinder <narinderd9@gmail.com>
2026-09-26 22:52:15 +0530

    backend/cors setupt

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -8,7 +8,11 @@ const app = express();
 
 //* Global middleware
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  }),
+);
 app.use(express.json());
 
 //* Health check
